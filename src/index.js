5675049d65eaab815c960742dfd83e2f83e946d7
Alok Kumar <alokk13799@gmail.com>
2023-01-30 11:41:53 +0530

    file path corrected

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 /** Importing files */
-import './index.css';
+import './styles/index.css';
 import App from './components/App';
 
 
